d73403668942ea334f282ca469de2f138a3f5ee5
SebastianMue <sebastian.muenchow@gmx.ch>
2019-11-09 18:51:09 -0500

    Add Rest services

---
+++ src/main/java/com/wbdv/projectbackend/controller/SearchController.java
@@ -0,0 +1,25 @@
+package com.wbdv.projectbackend.controller;
+
+import com.wbdv.projectbackend.model.AmazonItem;
+import com.wbdv.projectbackend.model.SearchResponse;
+import com.wbdv.projectbackend.serives.AmazonProductAPI;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class SearchController {
+
+    AmazonProductAPI amazonProductAPI = new AmazonProductAPI();
+
+    @GetMapping("/api/amazon/product/")
+    public List<SearchResponse> findByTitle(@RequestParam("searchText") String searchText) {
+        return amazonProductAPI.executeFulltextSearch(searchText);
+    }
+    @GetMapping("/api/amazon/product/{asin}")
+    public AmazonItem findByTitle(@PathVariable("asin") Integer asin) {
+        return amazonProductAPI.getItemDetail(asin);
+    }
+
+}
--- src/main/java/com/wbdv/projectbackend/serives/AmazonProductAPI.java
+++ src/main/java/com/wbdv/projectbackend/serives/AmazonProductAPI.java
@@ -3,19 +3,26 @@ package com.wbdv.projectbackend.serives;
 import com.wbdv.projectbackend.model.AmazonItem;
 import com.wbdv.projectbackend.model.SearchResponse;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AmazonProductAPI {
 
-    //Singleton
+    private SearchResponse[] searchResponses = new SearchResponse[]{new SearchResponse(1,"Produkt 1"),new SearchResponse(2,"Produkt 2"),new SearchResponse(3,"Produkt 3")};
+    private AmazonItem[] amazonItems = new AmazonItem[]{new AmazonItem(1,"title1","author1", "manu1", "book"),
+            new AmazonItem(2,"title2","author2", "manu2", "car"),
+            new AmazonItem(3,"title3","author3", "manu3", "laptop"),
+
+    };
 
     public List<SearchResponse> executeFulltextSearch(String searchText) {
-        return new ArrayList<>();
+        return Arrays.asList(searchResponses);
     }
 
     public AmazonItem getItemDetail(Integer asin) {
-        return new AmazonItem();
+        return Arrays.stream(amazonItems).filter(amazonItems -> amazonItems.getId().equals(asin)).findAny().orElse(null);
     }
 
 }
